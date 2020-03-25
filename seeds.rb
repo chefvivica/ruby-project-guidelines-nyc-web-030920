6d3e9be04e90d9ebb185c70410ff258f0d16c7a4
stephenverdi95 <stephenverdi152@gmail.com>
2020-03-25 09:29:58 -0400

    wrote seeds and commented out API request

--- seeds.rb
+++ seeds.rb
@@ -1,8 +1,22 @@
 require 'rest-client'
 require 'pry'
 require 'JSON'
-yelp_search = RestClient.get("https://api.yelp.com/v3/businesses/search")
-parsed_data = JSON.parse(yelp_search)
-parsed_data.each do |restaurant| 
-Restaurant.create(name: restaurant["name"], location: restaurant["location"])
-end
+# require_relative './config'
+# require_relative './app'
+User.destroy_all
+Restaurant.destroy_all
+Reservation.destroy_all
+# yelp_search = RestClient.get("https://api.yelp.com/v3/businesses/search")
+# parsed_data = JSON.parse(yelp_search)
+
+user1 = User.new(name: "User1")
+user2 = User.new(name: "User2")
+restaurant1 = Restaurant.new(name: "restaurant1name", location: "location1")
+restaurant2 = Restaurant.new(name: "restaurant2name", location: "location2")
+reservation1 = Reservation.new(user: user1, restaurant: restaurant1)
+reservation2 = Reservation.new(user: user2, restaurant: restaurant2)
+
+binding.pry
+# parsed_data.each do |restaurant| 
+# Restaurant.create(name: restaurant["name"], location: restaurant["location"])
+# end

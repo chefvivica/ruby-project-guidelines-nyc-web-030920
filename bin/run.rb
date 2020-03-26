0edbadc46f5d2e83ec9d40b6d418fd72e5e39577
Vivica <Ma>
2020-03-26 13:45:13 -0400

    something

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,6 @@ require_relative '../config/environment'
 puts "Welcome! Please enter your ZIP Code:"
 zip = gets.chomp
 results = Restaurant.all.select{|restaurant| restaurant.location == zip}
-names = results.each{|restaurant| restaurant.name}
+names = results.map{|restaurant| restaurant.name}
 puts "Here are the restaurants in your area:"
 puts names
